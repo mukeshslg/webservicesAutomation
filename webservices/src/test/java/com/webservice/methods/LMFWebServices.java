e164da2a9147365f25bc599f9db5dd0457e95f5a
PICKUNIDHI <PICKUNIDHI@192.168.1.4>
2018-11-01 20:27:24 +0530

    first commit

---
+++ webservices/src/test/java/com/webservice/methods/LMFWebServices.java
@@ -0,0 +1,38 @@
+package com.webservice.methods;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+
+public class LMFWebServices {
+
+	public static Response post(String URI,String strJSON) {
+		RequestSpecification requestSpecification= RestAssured.given().body(strJSON);
+		requestSpecification.contentType(ContentType.JSON);
+		Response response= requestSpecification.post(URI);
+		return response;
+		 
+	}
+	public static Response get(String URI) {
+		RequestSpecification requestSpecification=RestAssured.given();
+		requestSpecification.contentType(ContentType.JSON);
+		Response response=requestSpecification.get(URI);
+		return response;
+	}
+	
+	public static Response put(String URI,String strJSON) {
+		RequestSpecification requestSpecification=RestAssured.given().body(strJSON);
+		requestSpecification.contentType(ContentType.JSON);
+		Response response=requestSpecification.put(URI);
+		return response;
+		
+	}
+	public static Response delete(String URI,String strJSON) {
+		RequestSpecification requestSpecification=RestAssured.given().body(strJSON);
+		requestSpecification.contentType(ContentType.JSON);
+		Response response=requestSpecification.delete(URI);
+		return response;
+		
+	}
+}
